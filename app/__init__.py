ff28b732269eac6b747aa75a75db80ada76be5aa
Vanili <katevanili98@gmail.com>
2022-04-29 18:40:29 +0300

    Added configuration to the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
+from app.config import DevConfig
+
 #App initialization
 app = Flask(__name__,  template_folder='Templates')
+
+#Setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
