77d3a9ac665e7f19091bb23bdc93ca63661991b0
Vanili <katevanili98@gmail.com>
2022-04-29 21:46:42 +0300

    Modified the requests file

--- app/config.py
+++ app/config.py
@@ -5,6 +5,10 @@ class Config:
     '''
 
     API_KEY = "ac4fe71ff54045d781683a87a8bbad28"
+    NEWS_BASE_URL_SOURCES = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_BASE_EVERYTHING_URL = 'https://newsapi.org/v2/everything?domains={}&apiKey={}'
+    NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
 class ProdConfig(Config):
 
--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,9 @@
 from flask import FLask
 from newsapi.newsapi_client import NewsApiClient
+from app import app
+
+api_key=None
+base_url=None
+base_url_for_everything=None
+base_url_top_headlines=None
+base_source_list=None
