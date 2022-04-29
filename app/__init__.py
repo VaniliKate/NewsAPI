f87e1801f76e3c35e91921fc902da88731eed179
Vanili <katevanili98@gmail.com>
2022-04-29 19:07:14 +0300

    Added the requests file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from app.config import DevConfig
+from .config import DevConfig
 
 #App initialization
-app = Flask(__name__,  template_folder='Templates')
+app = Flask(__name__,  template_folder='Templates', instance_relative_config = True)
 
 #Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
